ac6e2ffc03a48f88a0bc497b2f6d7bd0e171c01c
seunmul <geonhab504@gmail.com>
2022-09-22 17:54:19 +0900

    cpp 추가

---
+++ hellocpp.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string a;
+int b[1004];
+
+int main(){
+
+    std::cout << "hi" << std::endl
+          << "my name is "
+          << "Psi" << std::endl;
+    return 0;
+}
